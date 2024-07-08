a83d9c7f40e0e3c1c1c375809764d93cf21ce5cd
João Pedro V <mastergamerjp06@gmail.com>
2024-07-08 03:06:59 +0000

    m7

--- JCRPROG/M7L/ex003.py
+++ JCRPROG/M7L/ex003.py
@@ -1,3 +1,17 @@
 # 3. Faça um programa que conta quantos caracteres únicos existem em uma string. A 
 # string 'Hello, world!' tem dez caracteres únicos, enquanto a string 'zzz' tem apenas 
 # um. Utilize um dicionário para resolver esse problema.
+
+texto = str(input("Digite um texto para verificar a quantidade de caractéres únicos: "))
+
+splitedText = list(texto)
+
+charList = []
+
+for char in splitedText:
+    if (char in charList):
+        continue
+    else:
+        charList.append(char)
+
+print("O texto digitado tem {} caracteres únicos".format(len(charList)))
--- JCRPROG/M7L/ex009.py
+++ JCRPROG/M7L/ex009.py
@@ -2,3 +2,26 @@
 # um dicionário, onde a chave é o nome do aluno. A entrada de dados deve terminar 
 # quando for lida uma string vazia como nome. Seu programa deve também calcular a 
 # média do aluno e exibir o nome e a média do aluno de maior média.
+
+alunos = {}
+
+melhorAluno = "Ninguém"
+melhorMedia = 0
+
+while True:
+    
+    nome = str(input("Digite o nome do aluno: \n"))
+
+    if (nome == "" or nome == " "):
+        break
+
+    nota1 = float(input("Digite a primeira nota do aluno: \n"))
+    nota2 = float(input("Digite a segunda nota do aluno: \n"))
+
+    if (nota1 + nota2)/2 > melhorMedia:
+        melhorMedia = (nota1 + nota2)/2
+        melhorAluno = nome
+
+    alunos[nome] = [nota1, nota2]
+
+print("A maior média foi {}, de {}".format(melhorMedia, melhorAluno))
