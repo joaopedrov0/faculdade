d2261ee2be707a2d2beeb3c6c1a5c3f679fd2b3e
João Pedro V <mastergamerjp06@gmail.com>
2024-07-21 00:23:39 +0000

    2 ex l8

--- JCRPROG/M8L/ex003.py
+++ JCRPROG/M8L/ex003.py
@@ -1,2 +1,17 @@
 # 3. Escreva um programa com uma função que, dado um número inteiro (n > 1), retorne 
 # uma lista com os fatores primos de n. 
+
+# Encontrar os números primos que, quando multiplicados, resultam no número digitado.
+
+def fatores_primos(n):
+    fatores = []
+    divisor = 2
+    while n > 1:
+        while n % divisor == 0:
+            fatores.append(divisor)
+            n //= divisor
+        divisor += 1
+    return fatores
+
+numero = int(input("Digite um número inteiro maior que 1: "))
+print("Os fatores primos de {} são: {}".format(numero, fatores_primos(numero)))
--- JCRPROG/M8L/ex004.py
+++ JCRPROG/M8L/ex004.py
@@ -4,3 +4,24 @@
 # �
 # �!
 #  (𝑚−𝑛)!𝑛!
+
+# Combinações onde a ORDEM NÃO IMPORTA e com ESPAÇO AMOSTRAL LIMITADO
+
+def fatorial(num):
+    if num == 0 or num == 1:
+        return 1
+    resultado = 1
+    for i in range(2, num + 1):
+        resultado *= i
+    return resultado
+
+def combinacoes(m, n):
+    return fatorial(m) // (fatorial(n) * fatorial(m - n))
+
+# Exemplo de uso
+m = int(input("Digite a quantidade de elementos do conjunto que você deseja analisar (conjunto inteiro): "))
+
+while True:
+    n = int(input("Digite o tamanho das combinações que você deseja analisar (deve ser maior do que o conjunto inteiro):    "))
+
+print("O número de combinações possíveis de {} elementos em grupos de {} elementos é: {}".format(m, n, combinacoes(m, n)))
